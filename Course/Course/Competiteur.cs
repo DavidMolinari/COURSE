97f3123991c0de445fcc252fae78d5b608b21b5a
David Molinari <contact@davidmolinari.fr>
2016-03-12 14:47:08 +0100

    ajout de la class competiteur

---
+++ Course/Course/Competiteur.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    public class Competiteur
+    {
+        private int NumeroCompetiteur { get; set; }
+        private string NomCompetiteur { get; set; }
+        private string PrenomCompetiteur { get; set; }
+        private int ClassementFIS { get; set; }
+        private int ClassementWCSL { get; set; }
+
+
+        public Competiteur(int num, string nom, string prenom, int classFIS, int classWCSL)
+        {
+            this.NumeroCompetiteur = num;
+            this.NomCompetiteur = nom;
+            this.PrenomCompetiteur = prenom;
+            this.ClassementFIS = classFIS;
+            this.ClassementWCSL = classWCSL;
+        }
+
+
+    }
+}
